40060c841757f535636d14c4b24b4f791d7ee0ef
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2023-12-05 19:04:48 +0100

    feat: add Cmake global and Cmake for the Client Manager, Room Manager and Server Manager

---
+++ engine/ClientManager/include/ClientManager.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Touhou
+** File description:
+** ClientManager
+*/
+
+#ifndef CLIENTMANAGER_HPP_
+#define CLIENTMANAGER_HPP_
+
+#include <iostream>
+
+#endif /* !CLIENTMANAGER_HPP_ */
---
+++ engine/ClientManager/src/Main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Touhou
+** File description:
+** Main
+*/
+
+#include "ClientManager.hpp"
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
