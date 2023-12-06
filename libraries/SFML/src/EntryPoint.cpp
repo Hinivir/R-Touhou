7075a7e9836eaeb3f4791305fa146d3146261ee6
Lipatant <virgile.berrier@gmail.com>
2023-12-06 17:28:56 +0100

    feat: adding basic SFML shared library compilation

---
+++ libraries/SFML/src/EntryPoint.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** libraries/SFML/src/EntryPoint.cpp
+** File description:
+** -
+*/
+
+extern "C" void entryPoint(void)
+{
+    return;
+}
+
+extern "C" void info(void)
+{
+    return;
+}
