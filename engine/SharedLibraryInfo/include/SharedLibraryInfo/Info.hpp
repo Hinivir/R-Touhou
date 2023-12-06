bbbda4b5f51209c1fc0ee0304d8f488da2508450
Lipatant <virgile.berrier@gmail.com>
2023-12-06 12:35:22 +0100

    feat: adding SharedLibraryInfo

---
+++ engine/SharedLibraryInfo/include/SharedLibraryInfo/Info.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** engine/SharedLibraryInfo/include/SharedLibraryInfo/Info.hpp
+** File description:
+** -
+*/
+
+/// @file
+/// @brief Structure used when loading shared libraries
+
+#include <string>
+
+namespace SharedLibraryInfo {
+
+enum class InfoType {
+    UNDEFINED,
+    GAME,
+    LIBRARY,
+    TOTAL,
+};
+
+using Info = struct Info_s {
+    SharedLibraryInfo::InfoType type;
+    std::string name;
+    std::string version;
+};
+
+}
