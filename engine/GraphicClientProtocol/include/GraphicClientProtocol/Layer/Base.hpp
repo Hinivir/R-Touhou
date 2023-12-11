1556ad9b897b7b28e890d9422d1f9d47fa016b06
Lipatant <virgile.berrier@gmail.com>
2023-12-11 18:28:24 +0100

    feat: adding color class and basic layer

---
+++ engine/GraphicClientProtocol/include/GraphicClientProtocol/Layer/Base.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** engine/GraphicClientProtocol/include/GraphicClientProtocol/Layer/Base.hpp
+** File description:
+** -
+*/
+
+/// @file
+/// @brief Base of all Layer class
+
+#pragma once
+
+namespace GraphicClientProtocol { namespace Layer {
+
+class Base {
+
+};
+
+}}
---
+++ engine/GraphicClientProtocol/src/Layer/Color.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2023
+** engine/GraphicClientProtocol/src/Layer/Color.cpp
+** File description:
+** -
+*/
+
+#include "GraphicClientProtocol/Layer/Color.hpp"
+
+GraphicClientProtocol::Layer::Color::Color(GraphicClientProtocol::Color const &color) :
+    color(color)
+{ }
