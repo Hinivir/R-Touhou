3d200a42ae714e077dc1048d768ee3f285438c2d
Pierre-Louis <pierre-louis.leroy@epitech.eu>
2023-12-13 16:00:36 +0100

    feat: push modifications for create a new branch room

---
+++ tests/Unit_tests.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** r-type-mirror
+** File description:
+** Server.cpp
+*/
+
+#include <criterion/criterion.h>
+
+Test(Rtype, Basic_test)
+{
+    cr_assert_eq(0, 0);
+}
