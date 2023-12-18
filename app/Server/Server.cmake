6d43030094bcabbd676946ae079b6b456b6bc7ec
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2023-12-18 15:05:57 +0100

    build: add CMake for the Server

---
+++ app/Server/Server.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.27.7)
+
+set(CMAKE_CXX_STANDARD 20)
+
+set(SERVER_BINARY_NAME "L-Type-Server")
+
+project(${SERVER_BINARY_NAME} LANGUAGES CXX)
+
+set(SERVER_FOLDER app/Server)
+
+set(SERVER_INCLUDE ${SERVER_FOLDER}/include)
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/${SERVER_FOLDER})
+
+include(${SERVER_FOLDER}/ServerSrc.cmake)
+
+add_executable(${SERVER_BINARY_NAME} ${SRC})
+
+target_include_directories(${SERVER_BINARY_NAME} PUBLIC ${SERVER_INCLUDE})
