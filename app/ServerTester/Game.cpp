cc530807d565db9377961cec795380e068112a79
Pierre-Louis <pierre-louis.leroy@epitech.eu>
2023-12-18 15:43:53 +0100

    feat: updating move square

--- app/ServerTester/Game.cpp
+++ app/ServerTester/Game.cpp
@@ -5,19 +5,21 @@
 ** Game.cpp
 */
 
+//compilation => g++ Game.cpp -o game -lsfml-graphics -lsfml-window -lsfml-system
+
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Game");
+    sf::RenderWindow window(sf::VideoMode(1200, 900), "SFML Game");
 
     sf::RectangleShape square1(sf::Vector2f(50, 50));
     sf::RectangleShape square2(sf::Vector2f(50, 50));
-
     square1.setFillColor(sf::Color::Red);
     square2.setFillColor(sf::Color::Red);
-
     square1.setPosition(100, 100);
-    square2.setPosition(300, 100);
+    square2.setPosition(100, 300);
+
+    float speed = 0.5f;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -25,6 +27,32 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            square1.move(-speed, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            square1.move(speed, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            square1.move(0, -speed);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            square1.move(0, speed);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+            square2.move(-speed, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            square2.move(speed, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+            square2.move(0, -speed);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            square2.move(0, speed);
         }
 
         window.clear();
