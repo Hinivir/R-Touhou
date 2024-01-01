22ff1adc20c4bcdc15ff0e6acf6ae14776a62d5c
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2024-01-01 04:50:35 +0100

    refactor: change parsing logic

--- app/Client/src/ParseMessage.cpp
+++ app/Client/src/ParseMessage.cpp
@@ -7,9 +7,10 @@
 
 #include "../include/Client.hpp"
 
-static const std::string RUN_GAME = "Game is ready! Let the fun begin!";
-static const std::string CONNECTED = "101:You are connected!";
-static const std::string CONNECTED_AS = "You are connected as player ";
+static const std::string RUN_GAME = "Game is ready! Let the fun begin!";//strcmp
+static const std::string CONNECTED = "101:You are connected!";//strcmp
+static const std::string CONNECTED_AS = "You are connected as player ";//strncmp
+static const std::string CONNECTED_PLAYER = " players connected";
 
 void Client::ParseMessage(const std::string message)
 {
@@ -18,6 +19,12 @@ void Client::ParseMessage(const std::string message)
         std::thread gameThread([&]() { this->runGame(); });
         gameThread.detach();
     }
+    if (message == CONNECTED)
+        this->player.isConnected = true;
+    if (message.substr(0, CONNECTED_AS.size()) == CONNECTED_AS)
+        this->player.player_number = std::stoi(message.substr(CONNECTED_AS.size(), 1));
+    if (strncmp(message.c_str(), CONNECTED_PLAYER.c_str(), 1) == 0)
+        this->other_player.player_number = std::stoi(message.substr(0, 1));
     if (this->inGame)
         this->handleMessageInGame(message);
 }
