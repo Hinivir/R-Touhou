eb2aa53e6d6cdc5df8aa0673a715d9b78e8a4aef
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2024-01-02 10:14:49 +0100

    refactor: changing const received messages

--- app/Client/src/ParseMessage.cpp
+++ app/Client/src/ParseMessage.cpp
@@ -9,22 +9,27 @@
 
 static std::size_t SPEED = 5;
 
-static const std::string RUN_GAME = "Game is ready! Let the fun begin!";//strcmp
-static const std::string CONNECTED = "101:You are connected!";//strcmp
-static const std::string CONNECTED_AS = "You are connected as player ";//strncmp
+static const std::string RUN_GAME = "104: You are ready!";
+static const std::string CONNECTED = "101: You are connected!";
+static const std::string CONNECTED_AS = "You are connected as player ";
 static const std::string CONNECTED_PLAYER = " players connected";
 
 void Client::ParseMessage(const std::string message)
 {
-    if (message == RUN_GAME && !this->inGame) {
+    if (strncmp(message.c_str(), RUN_GAME.c_str(), RUN_GAME.length()) == 0 && !this->inGame) {
+        std::cout << "Game is running" << std::endl;
         this->inGame = true;
         std::thread gameThread([&]() { this->runGame(); });
         gameThread.detach();
     }
-    if (message == CONNECTED)
+    if (strncmp(message.c_str(), RUN_GAME.c_str(), RUN_GAME.length()) == 0) {
         this->player.isConnected = true;
-    if (message.substr(0, CONNECTED_AS.size()) == CONNECTED_AS)
+        std::cout << "You are connected!!!!!" << std::endl;
+    }
+    if (message.substr(0, CONNECTED_AS.size()) == CONNECTED_AS) {
         this->player.player_number = std::stoi(message.substr(CONNECTED_AS.size(), 1));
+        std::cout << "You are player " << this->player.player_number << std::endl;
+    }
     if (strncmp(message.c_str(), CONNECTED_PLAYER.c_str(), 1) == 0)
         this->other_player.player_number = std::stoi(message.substr(0, 1));
     if (this->inGame)
