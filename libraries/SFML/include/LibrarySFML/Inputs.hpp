826e8f09aa2bb5e68c02fe0f97c56d8b4a916bd4
Lipatant <virgile.berrier@gmail.com>
2024-01-02 11:53:34 +0100

    feat: adding managing number keys, including from the numpad

--- libraries/SFML/include/LibrarySFML/Inputs.hpp
+++ libraries/SFML/include/LibrarySFML/Inputs.hpp
@@ -21,6 +21,28 @@ using KeyboardToInputList = struct KeyboardToInputList_s {
 };
 
 static const LibrarySFML::KeyboardToInputList KEYBOARD_TO_INPUT_LIST[] = {
+    // Numbers
+    {sf::Keyboard::Num0,    LType::InputList::KEY_0},
+    {sf::Keyboard::Num1,    LType::InputList::KEY_1},
+    {sf::Keyboard::Num2,    LType::InputList::KEY_2},
+    {sf::Keyboard::Num3,    LType::InputList::KEY_3},
+    {sf::Keyboard::Num4,    LType::InputList::KEY_4},
+    {sf::Keyboard::Num5,    LType::InputList::KEY_5},
+    {sf::Keyboard::Num6,    LType::InputList::KEY_6},
+    {sf::Keyboard::Num7,    LType::InputList::KEY_7},
+    {sf::Keyboard::Num8,    LType::InputList::KEY_8},
+    {sf::Keyboard::Num9,    LType::InputList::KEY_9},
+    {sf::Keyboard::Numpad0, LType::InputList::KEY_0},
+    {sf::Keyboard::Numpad1, LType::InputList::KEY_1},
+    {sf::Keyboard::Numpad2, LType::InputList::KEY_2},
+    {sf::Keyboard::Numpad3, LType::InputList::KEY_3},
+    {sf::Keyboard::Numpad4, LType::InputList::KEY_4},
+    {sf::Keyboard::Numpad5, LType::InputList::KEY_5},
+    {sf::Keyboard::Numpad6, LType::InputList::KEY_6},
+    {sf::Keyboard::Numpad7, LType::InputList::KEY_7},
+    {sf::Keyboard::Numpad8, LType::InputList::KEY_8},
+    {sf::Keyboard::Numpad9, LType::InputList::KEY_9},
+    // Arrows
     {sf::Keyboard::Left,    LType::InputList::LEFT},
     {sf::Keyboard::Right,   LType::InputList::RIGHT},
     {sf::Keyboard::Up,      LType::InputList::UP},
