cb9bd405bab24e3cc45e12e8ea9751037bd988f7
Lipatant <virgile.berrier@gmail.com>
2024-01-08 12:42:08 +0100

    feat: adding Components/ZIndex

---
+++ ecs/include/Components/ZIndex.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Touhou_Bootstrap
+** File description:
+** Position
+*/
+
+#ifndef POSITION_HPP_
+#define POSITION_HPP_
+
+namespace GameEngine
+{
+    using ZIndexValue = float;
+    struct ZIndex
+    {
+        GameEngine::ZIndexValue ZIndex;
+    };
+} // namespace GameEngine
+
+#endif /* !POSITION_HPP_ */
