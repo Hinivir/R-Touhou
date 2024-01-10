446f7f18db18c23d9e0160396087c2c37d361853
Pierre-Louis <pierre-louis.leroy@epitech.eu>
2024-01-10 13:38:10 +0100

    feat: adding ground up and down in gameplay

--- games/R-Type/src/Main.cpp
+++ games/R-Type/src/Main.cpp
@@ -27,7 +27,7 @@ GameEngine::Entity spawnMovableEntity(GameEngine::Registry &registry)
     GameEngine::Entity entity = spawnBaseEntity(registry);
 
     registry.addComponent<GameEngine::Controllable>(entity, GameEngine::Controllable{true});
-    registry.addComponent<GameEngine::Position>(entity, GameEngine::Position{0.0f, 0.0f});
+    registry.addComponent<GameEngine::Position>(entity, GameEngine::Position{0.0f, 500.0f});
     registry.addComponent<GameEngine::Velocity>(entity, GameEngine::Velocity{10.0f, 10.0f});
     registry.addComponent<GameEngine::Size>(entity, GameEngine::Size{50.0f, 50.0f});
     registry.addComponent<GameEngine::Sprite>(entity, GameEngine::Sprite{"../resources/R-Touhou/graphics/Player.png",sf::Sprite(),sf::Texture()});
@@ -64,8 +64,41 @@ GameEngine::Entity createBackgroundStar(GameEngine::Registry &registry, float wi
     registry.addComponent<GameEngine::Sprite>(backgroundStar, GameEngine::Sprite{"../resources/R-Touhou/graphics/BackgroundStar.jpg",sf::Sprite(),sf::Texture()});
     registry.addComponent<GameEngine::ZIndex>(backgroundStar, GameEngine::ZIndex{GAME_ENGINE_Z_INDEX_VALUE_LOWEST_VALUE});
     registry.addComponent<GameEngine::Projectile>(backgroundStar, GameEngine::Projectile{false});
+    registry.addComponent<GameEngine::Controllable>(backgroundStar, GameEngine::Controllable{false});
 
     return backgroundStar;
+}
+
+GameEngine::Entity createGroundDown(GameEngine::Registry &registry, float widthWindow, float heightWindow)
+{
+    GameEngine::Entity groundDown = registry.spawnEntity();
+
+    registry.addComponent<GameEngine::Drawable>(groundDown, GameEngine::Drawable{true});
+    registry.addComponent<GameEngine::Size>(groundDown, GameEngine::Size{1920.0f, 50.0f});
+    registry.addComponent<GameEngine::Position>(groundDown, GameEngine::Position{0.0f, 0.0f});
+    // registry.addComponent<GameEngine::Velocity>(groundDown, GameEngine::Velocity{10.0f, 0.0f});
+    registry.addComponent<GameEngine::Sprite>(groundDown, GameEngine::Sprite{"../resources/R-Touhou/graphics/Ground.png",sf::Sprite(),sf::Texture()});
+    registry.addComponent<GameEngine::ZIndex>(groundDown, GameEngine::ZIndex{GAME_ENGINE_Z_INDEX_VALUE_LOWEST_VALUE + 1});
+    registry.addComponent<GameEngine::Projectile>(groundDown, GameEngine::Projectile{false});
+    registry.addComponent<GameEngine::Controllable>(groundDown, GameEngine::Controllable{false});
+
+    return groundDown;
+}
+
+GameEngine::Entity createGroundUp(GameEngine::Registry &registry, float widthWindow, float heightWindow)
+{
+    GameEngine::Entity groundUp = registry.spawnEntity();
+
+    registry.addComponent<GameEngine::Drawable>(groundUp, GameEngine::Drawable{true});
+    registry.addComponent<GameEngine::Size>(groundUp, GameEngine::Size{1920.0f, 50.0f});
+    registry.addComponent<GameEngine::Position>(groundUp, GameEngine::Position{0.0f, 1030.0f});
+    // registry.addComponent<GameEngine::Velocity>(groundUp, GameEngine::Velocity{10.0f, 0.0f});
+    registry.addComponent<GameEngine::Sprite>(groundUp, GameEngine::Sprite{"../resources/R-Touhou/graphics/Ground.png",sf::Sprite(),sf::Texture()});
+    registry.addComponent<GameEngine::ZIndex>(groundUp, GameEngine::ZIndex{GAME_ENGINE_Z_INDEX_VALUE_LOWEST_VALUE + 2});
+    registry.addComponent<GameEngine::Projectile>(groundUp, GameEngine::Projectile{false});
+    registry.addComponent<GameEngine::Controllable>(groundUp, GameEngine::Controllable{false});
+
+    return groundUp;
 }
 
 GameEngine::Entity createScore(GameEngine::Registry &registry)
@@ -116,6 +149,8 @@ int main(void)
 
     GameEngine::Entity movableEntity = spawnMovableEntity(registry);
     GameEngine::Entity backgroundStar = createBackgroundStar(registry, widthWindow, heightWindow);
+    GameEngine::Entity groundDown = createGroundDown(registry, widthWindow, heightWindow);
+    GameEngine::Entity groundUp = createGroundUp(registry, widthWindow, heightWindow);
     GameEngine::Entity score = createScore(registry);
 
     for (int i = 0; i < 5; ++i)
