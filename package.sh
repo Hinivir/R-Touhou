1acc12e9eeb78e6decc4584b25ede068a730beef
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-01-14 17:30:30 +0100

    ci: Refactor file paths in package.sh

--- package.sh
+++ package.sh
@@ -9,6 +9,6 @@ rm -rf build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DINSTALL=""
 cd build
 cpack --config CPackConfig.cmake
-
-mv L-Type-0.1.1-Linux.sh ../artifact/L-Type-$version-Linux.sh
-mv L-Type-0.1.1-Linux.zip ../artifact/L-Type-$version-Linux.zip
+cd ..
+mv build/L-Type-0.1.1-Linux.sh artifact/L-Type-$version-Linux.sh
+mv build/L-Type-0.1.1-Linux.zip artifact/L-Type-$version-Linux.zip
