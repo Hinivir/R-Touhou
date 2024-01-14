8591a992986ba10e6a85eb6fe08b0dcd9452448a
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-01-14 17:25:08 +0100

    ci: Refactor package.sh script to improve readability

--- package.sh
+++ package.sh
@@ -7,7 +7,8 @@ rm -rf build
 
 # Build the project
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DINSTALL=""
-cd build && cpack --config CPackConfig.cmake
+cd build
+cpack --config CPackConfig.cmake
 
 mv L-Type-0.1.1-Linux.sh ../artifact/L-Type-$version-Linux.sh
 mv L-Type-0.1.1-Linux.zip ../artifact/L-Type-$version-Linux.zip
