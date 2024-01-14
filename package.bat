c3b2bedaaff77c972b4eaaef0e44b1cdfbb5050d
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-01-14 17:18:08 +0100

    ci: Update package.bat script to include Release configuration

--- package.bat
+++ package.bat
@@ -5,7 +5,9 @@ set version=%1
 del /q build\CMakeCache.txt
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DINSTALL=""
+cmake --build .\build --config Release
 
-cd build && cpack --config CPackConfig.cmake
+cd build
+cpack --config CPackConfig.cmake -C Release
 move "L-Type-0.1.1-win64.zip" "..\artifacts\L-Type-$verison-Windows.zip"
 move "L-Type-0.1.1-win64.exe" "..\artifacts\L-Type-$verison-Windows.exe"
