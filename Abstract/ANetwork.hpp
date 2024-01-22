6430d52ee7eed27ae3c376d755f7f247140e57e9
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2024-01-22 10:46:35 +0100

    feat: adding Abstract class file

---
+++ Abstract/ANetwork.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** r-type-mirror
+** File description:
+** ANetwork.hpp
+*/
+
+#ifndef ANETWORK_HPP
+    #define ANETWORK_HPP
+
+#include <iostream>
+
+class ANetwork {
+
+};
+#endif
