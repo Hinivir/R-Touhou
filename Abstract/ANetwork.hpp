c7e3a40ce432aea0a08acc945804fdcb044e0ced
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2024-01-22 11:25:50 +0100

    feat: completing ANetwork

--- Abstract/ANetwork.hpp
+++ Abstract/ANetwork.hpp
@@ -9,8 +9,36 @@
     #define ANETWORK_HPP
 
 #include <iostream>
+#include <array>
+
+#include <asio.hpp>
+
+template <typename senderMessage>
+struct inGame_message {
+    std::size_t index;
+    senderMessage &message;
+};
 
 class ANetwork {
+    protected:
+        std::array<char, 2048> buffer;
+        std::string ip;
+        std::string port;
+        //vector pour server
+        //endpoint pour client
 
+    public:
+        ANetwork(const std::string ip, const std::string port);
+        ~ANetwork();
+
+        void sendMessageString(const std::string &message, asio::ip::udp::endpoint &endpoint);//override
+
+        template<typename T>
+        void sendMessageStruct(const T &message, asio::ip::udp::endpoint &endpoint);//override
+
+        std::string receiveMessageString();//override
+
+        template<typename Component>
+        Component receiveMessageStruct();//override
 };
 #endif
