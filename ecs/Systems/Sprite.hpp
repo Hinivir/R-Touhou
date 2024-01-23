5e4979f2db5219d8132bbe6c713034003ed6f2da
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-01-23 14:54:08 +0100

    style: style the code to be at the coding style for the project

--- ecs/Systems/Sprite.hpp
+++ ecs/Systems/Sprite.hpp
@@ -12,41 +12,42 @@
 #include "Components/Sprite.hpp"
 #include "Macros/Systems.hpp"
 
-namespace GameEngine {
-
-namespace System {
-
-void sprite(GameEngine::Registry &REGISTRY_DEFAULT_NAME)
+namespace GameEngine
 {
-    EXTRACT_COMPONENT(GameEngine::Sprite, sprites);
-    EXTRACT_COMPONENT_CONST(GameEngine::Size, sizes);
 
-    for (size_t i = 0; i < sprites.size(); ++i) {
-        if (std::find(r.garbageEntities.begin(), r.garbageEntities.end(), i) != r.garbageEntities.end())
-            continue;
-        // Sprite - Continues if sprite is undefined or if it has no path
-        FROM_COMPONENT_TO_VARIABLE(sprites, i, spriteComponent, hasSprite);
-        if (!hasSprite)
-            continue;
-        GameEngine::Sprite &sprite = spriteComponent.value();
-        std::string const &path = sprite.path;
-        if (path == "")
-            continue;
+    namespace System
+    {
 
-        sprite.texture.loadFromFile(sprite.path);
-        sprite.sprite.setTexture(sprite.texture);
+        void sprite(GameEngine::Registry &REGISTRY_DEFAULT_NAME)
+        {
+            EXTRACT_COMPONENT(GameEngine::Sprite, sprites);
+            EXTRACT_COMPONENT_CONST(GameEngine::Size, sizes);
 
-        // Size
-        FROM_COMPONENT_TO_VARIABLE_CONST(sizes, i, size, hasSize);
-        if (hasSize)
-            sprite.sprite.setScale(size.value().width / sprite.texture.getSize().x,
-                size.value().height / sprite.texture.getSize().y);
-    }
-}
+            for (size_t i = 0; i < sprites.size(); ++i) {
+                if (std::find(r.garbageEntities.begin(), r.garbageEntities.end(), i) != r.garbageEntities.end())
+                    continue;
+                // Sprite - Continues if sprite is undefined or if it has no path
+                FROM_COMPONENT_TO_VARIABLE(sprites, i, spriteComponent, hasSprite);
+                if (!hasSprite)
+                    continue;
+                GameEngine::Sprite &sprite = spriteComponent.value();
+                std::string const &path = sprite.path;
+                if (path == "")
+                    continue;
 
-}
+                sprite.texture.loadFromFile(sprite.path);
+                sprite.sprite.setTexture(sprite.texture);
 
-}
+                // Size
+                FROM_COMPONENT_TO_VARIABLE_CONST(sizes, i, size, hasSize);
+                if (hasSize)
+                    sprite.sprite.setScale(size.value().width / sprite.texture.getSize().x,
+                        size.value().height / sprite.texture.getSize().y);
+            }
+        }
 
+    } // namespace System
+
+} // namespace GameEngine
 
 #endif
