403524d1e83a34b2546bd1fa2c72bdec341d7bf3
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-01-25 16:16:54 +0100

    build: Remove unused file from ClientSrc.cmake

--- Client/ClientSrc.cmake
+++ Client/ClientSrc.cmake
@@ -2,5 +2,4 @@ set(CLIENT_SRC
         ${CLIENT_FOLDER}/main.cpp
         ${CLIENT_FOLDER}/src/ActionFunction.cpp
         ${CLIENT_FOLDER}/src/ParseMessage.cpp
-        ${CLIENT_FOLDER}/src/Client.cpp
-        ${CLIENT_FOLDER}/src/Game.cpp)
+        ${CLIENT_FOLDER}/src/Client.cpp)
