ad8a3576b0b643e65a1b1924c5143a7d7950da10
Lipatant <virgile.berrier@gmail.com>
2024-01-25 14:18:41 +0100

    feat: adding Events to the ECS

--- ecs/Macros/Systems.hpp
+++ ecs/Macros/Systems.hpp
@@ -12,6 +12,10 @@
 
 #define REGISTRY_DEFAULT_NAME r
 
+#define CLEAR_EVENT(EVENT) r.clearEvent<EVENT>()
+
+#define CLEAR_EVENT_FROM(EVENT, REGISTRY) registry.clearEvent<EVENT>()
+
 #define DO_COMPONENT_CONTAINS_AT(COMPONENT, ID) (ID < COMPONENT.size() && COMPONENT[ID].has_value())
 
 #define EXTRACT_COMPONENT(COMPONENT, VARIABLE) auto &VARIABLE = REGISTRY_DEFAULT_NAME.getComponent<COMPONENT>()
@@ -21,6 +25,14 @@
 #define EXTRACT_COMPONENT_FROM(COMPONENT, VARIABLE, REGISTRY) auto &VARIABLE = REGISTRY.getComponent<COMPONENT>()
 #define EXTRACT_COMPONENT_FROM_CONST(COMPONENT, VARIABLE, REGISTRY)                                                    \
     auto const &VARIABLE = REGISTRY.getComponent<COMPONENT>()
+
+#define EXTRACT_EVENT(EVENT, VARIABLE) auto &VARIABLE = REGISTRY_DEFAULT_NAME.getComponent<EVENT>()
+#define EXTRACT_EVENT_CONST(EVENT, VARIABLE)                                                                   \
+    auto const &VARIABLE = REGISTRY_DEFAULT_NAME.getComponent<EVENT>()
+
+#define EXTRACT_EVENT_FROM(EVENT, VARIABLE, REGISTRY) auto &VARIABLE = REGISTRY.getComponent<EVENT>()
+#define EXTRACT_EVENT_FROM_CONST(EVENT, VARIABLE, REGISTRY)                                                    \
+    auto const &VARIABLE = REGISTRY.getComponent<EVENT>()
 
 #define FROM_COMPONENT_TO_VARIABLE(COMPONENT, ID, VARIABLE, VARIABLE_HAS)                                              \
     bool const VARIABLE_HAS = DO_COMPONENT_CONTAINS_AT(COMPONENT, ID);                                                 \
