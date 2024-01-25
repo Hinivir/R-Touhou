846c5701c549d72c8eb06392c41fff383e5916c6
Lipatant <virgile.berrier@gmail.com>
2024-01-25 13:39:48 +0100

    feat: adding GameEngine::GenericEventBase

---
+++ ecs/Events/GenericEventBase.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** L-Type-Game-RTouhou
+** File description:
+** Events/GenericEvent.hpp
+*/
+
+#ifndef L_TYPE_GAME_RTOUHOU_EVENT_GENERIC_EVENT_BASE_HPP
+#define L_TYPE_GAME_RTOUHOU_EVENT_GENERIC_EVENT_BASE_HPP
+
+namespace GameEngine
+{
+    template <char const *UNIQUE_NAME>
+    struct GenericEventBase {
+    };
+} // namespace GameEngine
+
+#endif // L_TYPE_GAME_RTOUHOU_EVENT_GENERIC_EVENT_BASE_HPP
