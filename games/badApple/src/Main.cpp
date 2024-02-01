6f2c3b2ec98d04edc87966ae3b552f6041b5995b
Lipatant <virgile.berrier@gmail.com>
2024-02-01 14:54:40 +0100

    refactor: removing unused components and systems for Bad Apple

--- games/badApple/src/Main.cpp
+++ games/badApple/src/Main.cpp
@@ -7,32 +7,9 @@
 
 #include "Registry.hpp"
 #include "Systems.hpp"
-#include "Systems/Draw.hpp"
-#include "Systems/Sprite.hpp"
 #include <SFML/Audio/Music.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
-namespace MyGame
-{
-    struct Gravity
-    {
-        bool isAffectedByGravity = false;
-        float weigth = 0;
-    };
-} // namespace MyGame
-
-void systemGravity(GameEngine::Registry &r)
-{
-    EXTRACT_COMPONENT(GameEngine::Position, positions);
-    EXTRACT_COMPONENT_CONST(MyGame::Gravity, gravities);
-    for (std::size_t i = 0; i < positions.size(); ++i) {
-        FROM_COMPONENT_TO_VARIABLE(positions, i, position, hasPosition);
-        FROM_COMPONENT_TO_VARIABLE_CONST(gravities, i, gravity, hasGravity);
-        if (hasPosition && hasGravity && gravity.value().isAffectedByGravity)
-            position.value().y += gravity.value().weigth;
-    }
-}
 
 int main(void)
 {
@@ -44,18 +21,6 @@ int main(void)
     GameEngine::Registry registry(nbRegistry);
     GameEngine::SystemGroup system;
     // Registering components for system.spriteSystem and system.drawSystem
-    registry.registerComponent<GameEngine::Color>();
-    registry.registerComponent<GameEngine::Controllable>();
-    registry.registerComponent<GameEngine::Drawable>();
-    registry.registerComponent<GameEngine::Outline>();
-    registry.registerComponent<GameEngine::Position>();
-    registry.registerComponent<GameEngine::Size>();
-    registry.registerComponent<GameEngine::Sprite>();
-    registry.registerComponent<GameEngine::SpriteTextureAnimation>();
-    registry.registerComponent<GameEngine::SpriteTextureRect>();
-    registry.registerComponent<GameEngine::Text>();
-    registry.registerComponent<GameEngine::ZIndex>();
-    registry.registerComponent<MyGame::Gravity>();
     // Music
     if (music.openFromFile("badApple/res/BA.wav"))
         music.play();
@@ -67,12 +32,6 @@ int main(void)
         while (window.pollEvent(event))
             if (event.type == sf::Event::Closed)
                 window.close();
-        // Initializing sprite textures
-        GameEngine::System::sprite(registry);
-        // Our gravity system
-        systemGravity(registry);
-        // Drawing entities on screen
-        GameEngine::System::draw(registry, window);
         // Displaying SFML window, then clearing it
         window.display();
         window.clear();
